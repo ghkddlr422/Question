c10c146b73522f8fe0c930e82226ff13a4c2167e
ghkddlr422 <66246456+ghkddlr422@users.noreply.github.com>
2020-06-05 17:54:38 +0900

    문제 1번~ 3번
    문제 1번~ 2번 진행중...

---
+++ Questions.java
@@ -0,0 +1,30 @@
+
+public class Questions {
+
+	public static void main(String[] args) {
+		//Q.1 정수형 변수 2개를 만들고, 그 2개에 임의의 값을 넣은 후 합을 출력해라.
+		//Q.2 문자 1개의 변수 2개를 만들고 그 2개에 임의의 문자를 넣은 후 출력해라.
+		//Q.3 문자 1개의 변수를 만들고 임의의 값을 넣은 후, 아스키코드(정수)값으로 바꿔서 출력해라.
+		
+		/*int i = 10;						//Q.1
+		int j = 20;
+		System.out.println("i 와 j 의 합 = ");
+		System.out.println(i+"와" +j+"의 합=" + (i +j));*/
+		
+
+		
+		/* byte a = 0;
+		short b = 0;
+		int c = 0;
+		long d = 0L;
+		float e = 0f;
+		double f = 0;
+		char g = ' ';
+		String h = ""; */
+		
+		char 소 = 'D';
+		char 닭 = '알';
+		System.out.println(""+소+닭);
+	}
+
+}
